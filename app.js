5d5f704b54b9fb48a123ac3b0fcf9db89230983d
DENN87 <denis.schiopu@yahoo.com>
2022-02-28 14:49:07 -0800

    Minor CSS tweaks.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Routes here */
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log("Server started on 3000.");
+});
