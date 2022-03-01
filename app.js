2d5d61c78c096b31f9cbd31451d531c7c153a530
DENN87 <denis.schiopu@yahoo.com>
2022-02-28 16:42:17 -0800

    Added mongoose-encryption package to encrypt user password saved in mongoDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const mongooseEncryption = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,11 +13,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /* Mongoose connnection */
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-/* Create userSchema */
-const userSchema = {
+/* Create userSchema accorind to mongoose Schema() */
+const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
-};
+});
+
+const secret = "VERY_SECRET_EXPRESSION";
+userSchema.plugin(mongooseEncryption, {
+	secret: secret,
+	encryptedFields: ["password"],
+});
+// including the field encryptedFields: [] will be the only encrypted field (not the username)
+
 /*  use userSchema to setup a mongoose model */
 const User = new mongoose.model("User", userSchema);
 
@@ -31,11 +40,15 @@ app.route("/login")
 	.post((req, res) => {
 		const userName = req.body.username;
 		const userPass = req.body.password;
-		/* Looking for a user into MongoDB with amatching username and password field  from EJS file*/
+		/* Looking for a user into MongoDB with a matching username from EJS file*/
 		User.findOne({ email: userName }, (err, foundUser) => {
 			if (!err) {
 				if (foundUser) {
+					// found a user
 					if (foundUser.password === userPass) {
+						// mongooseEncryption package will automatically encrypt and decrypt -
+						// the password field when having some logic.
+						// checking foundUser password equals to input password from EJS file.
 						res.render("secrets");
 					}
 				}
@@ -54,9 +67,9 @@ app.route("/register")
 		const newUser = new User({
 			/* saving the fields we are getting from POST action of the form in EJS file */
 			email: req.body.username,
-			// username is the NAME field from EJS file of the coresping input
+			// username is the NAME field from EJS file of the corresponding input
 			password: req.body.password,
-			// password is the NAME field from EJS file of the coresping input
+			// password is the NAME field from EJS file of the corresponding input
 		});
 		/* Saving the newUser created above to the mongoDB */
 		newUser.save((err) => {
